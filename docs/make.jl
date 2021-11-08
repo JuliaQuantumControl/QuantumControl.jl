672ee30116bec6ed73f314eec00f518734ec7ea9
Michael Goerz <goerz@stanford.edu>
2021-11-08 01:20:21 -0500

    Do not define "repo" in makedocs
    Defining an explicit "repo" prevents "Source" links in the API from
    working for any function that isn't defined in the given repo. Without
    that definition, links work automatically!

--- docs/make.jl
+++ docs/make.jl
@@ -14,7 +14,6 @@ include("generate_api.jl")
 
 makedocs(;
     authors="Michael Goerz <mail@michaelgoerz.net>, Alastair Marshall <alastair@nvision-imaging.com>, and contributors",
-    repo="https://github.com/JuliaQuantumControl/QuantumControl.jl/blob/{commit}{path}#{line}",
     sitename="QuantumControl.jl",
     format=Documenter.HTML(;
         prettyurls=true,
