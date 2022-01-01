c1e6c5e71429f88fd65d980f97cc663ccdd70a8b
Michael Goerz <goerz@stanford.edu>
2022-01-01 16:38:21 -0500

    Add private print_versions function
    This helps with testing/debugging

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,12 @@
 using QuantumControl
+using IOCapture
 using Test
 
 @testset "QuantumControl.jl" begin
-    # Write your tests here.
+    captured = IOCapture.capture() do
+        QuantumControl.print_versions()
+    end
+    println(captured.output)
+    @test occursin("QuantumControlBase", captured.output)
+    @test occursin("Krotov", captured.output)
 end
